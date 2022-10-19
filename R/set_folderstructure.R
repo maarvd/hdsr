dc225146cd4918ecbe3d1a35970be970de974a8e
maarvd <maarten.vandoorn@nmi-agro.nl>
2022-10-19 13:54:50 +0200

    more functions

---
+++ R/set_folderstructure.R
@@ -0,0 +1,25 @@
+#' Set folder structure for extracting excel data
+#'
+#'
+#' @param datafolder folder where all excel waterbalances are located
+#'
+#'
+#' @export
+set_folderstructure <- function(datafolder){
+  #list excelfiles
+  filelist <- list.files(datafolder, pattern = ".xls$|.xlsm$")
+
+  #tidy
+  filelist <- gsub(".xls$|.xlsm$", "", filelist)
+
+  #create output folder
+  if(!dir.exists("output")){dir.create("output")}
+
+  #for each, create folder sturcture
+  lapply(filelist, FUN = function(x){
+    if(!dir.exists(paste0("output/", x))){dir.create(paste0("output/",x))}
+    if(!dir.exists(paste0("output/",x, "/raw"))){dir.create(paste0("output/", x, "/raw"))}
+    if(!dir.exists(paste0("output/",x, "/formatted"))){dir.create(paste0("output/", x, "/formatted"))}
+    if(!dir.exists(paste0("output/",x, "/spatial"))){dir.create(paste0("output/", x, "/spatial"))}
+  })
+}
