03042764216d01d4cafe7dec16c86572e70aee4e
maarvd <maarten.vandoorn@nmi-agro.nl>
2022-10-26 14:32:34 +0200

    add afvoergebieden as datafile

---
+++ R/afvoergebieden-data.R
@@ -0,0 +1,10 @@
+#' Afvoergebieden
+#'
+#' Spatial file containing locations of all afvoergebieden with ID. Renamed IDs to match waterbalances
+#'
+#' @docType data
+#'
+#' @usage data(afvoergebieden)
+#'
+#' @example
+#' data(afvoergebieden)
