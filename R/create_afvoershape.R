b8dfb8a657983ddca68d5801965f67cfdfbae1f2
maarvd <maarten.vandoorn@nmi-agro.nl>
2022-10-26 15:31:00 +0200

    add check function to create_afvoershape

--- R/create_afvoershape.R
+++ R/create_afvoershape.R
@@ -5,10 +5,13 @@
 #' @param waterbalance code of waterbalance (e.g. KRWO_04_Kockengen)
 #'
 #' @import data.table
+#' @import ggplot2
 #' @importFrom sf st_transform st_as_sf st_write
+#' @importFrom ggspatial annotation_map_tile
+#' @importFrom gridExtra grid.arrange
 #'
 #' @export
-create_afvoershape <- function(sf, waterbalance){
+create_afvoershape <- function(sf, waterbalance, check){
   #load afvoer.shape
   afvoer.shape <- sf |> st_transform(28992) |> as.data.table()
 
@@ -19,13 +22,27 @@ create_afvoershape <- function(sf, waterbalance){
   #select rel shape
   rel.shape <- afvoer.shape[afgb_id == waterbalans.id] |> st_as_sf()
 
-  #yes no
-  if(askYesNo(msg = paste0("Shapefile ", rel.shape$naam, " geselecteerd voor waterbalans ", waterbalance, ". Klopt dit?")) == TRUE){
-    #save shape
+  #if check is desired
+  if(check == TRUE){
+    #new dev
+    print(dev.new(1))
+
+    #visualize
+    plot1 <- ggplot(afvoergebieden) + geom_sf(fill = "white") + theme_void() +
+      geom_sf(data = rel.shape, fill = "red")
+
+    plot2 <- ggplot(rel.shape) + ggspatial::annotation_map_tile() + geom_sf(fill = NA, col = "red", linetype = "dashed", size = 1) + theme_void()
+
+    gridExtra::grid.arrange(plot1, plot2, ncol = 2, nrow = 1)
+
+    if(askYesNo(msg = paste0("Shapefile ", rel.shape$naam, " geselecteerd voor waterbalans ", waterbalance, ". Klopt dit?")) == TRUE){
+
+      #save shape
+      st_write(rel.shape, dsn = paste0("output/", waterbalance, "/spatial/shape.gpkg"), append = FALSE)
+    } else{
+      warning(paste0("Geen ruimtelijk bestand opgeslagen voor waterbalans ", waterbalance))
+    }
+  } else if(check == FALSE){
     st_write(rel.shape, dsn = paste0("output/", waterbalance, "/spatial/shape.gpkg"), append = FALSE)
-  } else{
-    warning(paste0("Geen ruimtelijk bestand opgeslagen voor waterbalans ", waterbalance))
   }
-
-  }
-
+}
