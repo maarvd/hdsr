e511bc03504e8a79a4e471200cb358ddb43b933e
maarvd <maarten.vandoorn@nmi-agro.nl>
2022-10-26 14:56:00 +0200

    adjustments in retrieving spatial data

--- R/create_afvoershape.R
+++ R/create_afvoershape.R
@@ -1,12 +1,11 @@
 #' Subset afvoershape of waterbalance of interest
 #'
 #'
-#' @param sf shapefile containing all afvoergebieden
+#' @param sf shapefile containing all afvoergebieden (retrieve from data(afvoergebieden))
 #' @param waterbalance code of waterbalance (e.g. KRWO_04_Kockengen)
 #'
 #' @import data.table
 #' @importFrom sf st_transform st_as_sf st_write
-#' @importFrom stringr str_pad
 #'
 #' @export
 create_afvoershape <- function(sf, waterbalance){
@@ -15,12 +14,18 @@ create_afvoershape <- function(sf, waterbalance){
 
   #convert filename to waterbalans.id
   waterbalans.id <- sapply(strsplit(waterbalance, "_"), `[`, 2)
-  waterbalans.id <- str_pad(waterbalans.id, 4, pad = "0")
+  waterbalans.id <- paste0("KRW_", waterbalans.id)
 
   #select rel shape
-  rel.shape <- afvoer.shape[CODE == paste0("AFVGEB", waterbalans.id)] |> st_as_sf()
+  rel.shape <- afvoer.shape[afgb_id == waterbalans.id] |> st_as_sf()
 
-  #save shape
-  st_write(rel.shape, dsn = paste0("output/", waterbalance, "/spatial/shape.gpkg"), append = FALSE)
-}
+  #yes no
+  if(askYesNo(msg = paste0("Shapefile ", rel.shape$naam, " geselecteerd voor waterbalans ", waterbalance, ". Klopt dit?")) == TRUE){
+    #save shape
+    st_write(rel.shape, dsn = paste0("output/", waterbalance, "/spatial/shape.gpkg"), append = FALSE)
+  } else{
+    warning(paste0("Geen ruimtelijk bestand opgeslagen voor waterbalans ", waterbalance))
+  }
+
+  }
 
--- R/create_seepage.R
+++ R/create_seepage.R
@@ -1,26 +1,38 @@
 #' Mask seepage raster to area waterbalance of interest (run create_afvoershape first)
 #'
 #'
-#' @param rast raster of LHM4.1 seepage
 #' @param waterbalance code of waterbalance (e.g. KRWO_04_Kockengen)
 #'
-#' @importFrom sf st_read
-#' @importFrom terra rast crop vect mask writeRaster
+#' @importFrom sf st_read st_bbox st_transform
 #'
 #' @export
-create_seepage <- function(rast, waterbalance){
-  #read shape
-  aoi <- st_read(paste0("output/", waterbalance, "/spatial/shape.gpkg"))
+create_seepage <- function(waterbalance){
+  #read geopackage
+  sf <- st_read(paste0("output/", waterbalance, "/spatial/shape.gpkg"))
 
-  #load seepage
-  seep <- terra::rast(rast)
+  #transform to 28992 to be sure
+  sf.transformed <- st_transform(sf, 28992)
 
-  #crop
-  seep <- terra::crop(seep, terra::vect(aoi))
+  #create bbox
+  bbox <- st_bbox(sf.transformed) |> paste0(collapse = ",")
 
-  #mask
-  seep <- terra::mask(seep, terra::vect(aoi))
+  #seepage url (LHM4.1 kwel-inzijging 2011-2018)
+  seep_url <- paste0("https://modeldata-nhi-data.deltares.nl/geoserver/LHM/ows?",
+                     "service=WCS&version=1.0.0",
+                     "&request=GetCoverage",
+                     "&coverage=LHM%3Akwel_mmd",
+                     "&bbox=", bbox,
+                     "&width=1200",
+                     "&height=1300",
+                     "&crs=EPSG%3A28992",
+                     "&format=geotiff")
 
-  #write
-  terra::writeRaster(seep, filename = paste0("output/", waterbalance, "/spatial/seepage.tiff"), overwrite = TRUE)
+  #save tiff
+  download.file(url = seep_url,
+                destfile = paste0("output/", waterbalance, "/spatial/LHM4.1_seepinf_2011-2018_mmday.tiff"))
+
+
+
 }
+
+
